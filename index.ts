cbaf07b3682737ee2ff8d612f5a5e2d64d5a657d
junaidkhan-aardy <junaid@aardy.com>
2021-12-03 12:25:03 +0000

    acl

--- index.ts
+++ index.ts
@@ -3,7 +3,14 @@ import * as awsx from "@pulumi/awsx"
 import * as pulumi from "@pulumi/pulumi"
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket")
+const bucket = new aws.s3.Bucket("my-bucket", { acl: "public" })
+
+// const examplebucketObject = new aws.s3.BucketObject("examplebucketObject", {
+//   key: "someobject",
+//   bucket: examplebucket.id,
+//   source: new pulumi.asset.FileAsset("index.html"),
+//   kmsKeyId: examplekms.arn,
+// });
 
 // Export the name of the bucket
 export const bucketName = bucket.id
