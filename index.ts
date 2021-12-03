7e8eb62628a3aa3aa1668a49333af513fa9e7bec
junaidkhan-aardy <junaid@aardy.com>
2021-12-03 12:30:43 +0000

    s3 image object

--- index.ts
+++ index.ts
@@ -5,12 +5,11 @@ import * as pulumi from "@pulumi/pulumi"
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("my-bucket", { acl: "public-read" })
 
-// const examplebucketObject = new aws.s3.BucketObject("examplebucketObject", {
-//   key: "someobject",
-//   bucket: examplebucket.id,
-//   source: new pulumi.asset.FileAsset("index.html"),
-//   kmsKeyId: examplekms.arn,
-// });
+const examplebucketObject = new aws.s3.BucketObject("bg-image", {
+  key: "bg-image-1",
+  bucket: bucket.id,
+  source: new pulumi.asset.FileAsset("images/Image.png"),
+})
 
 // Export the name of the bucket
 export const bucketName = bucket.id
