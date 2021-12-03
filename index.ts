b259a93fbcf87d321f816a2ed3f514bc02a1c6e8
junaidkhan-aardy <junaid@aardy.com>
2021-12-03 12:26:49 +0000

    public read

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import * as awsx from "@pulumi/awsx"
 import * as pulumi from "@pulumi/pulumi"
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket", { acl: "public" })
+const bucket = new aws.s3.Bucket("my-bucket", { acl: "public-read" })
 
 // const examplebucketObject = new aws.s3.BucketObject("examplebucketObject", {
 //   key: "someobject",
